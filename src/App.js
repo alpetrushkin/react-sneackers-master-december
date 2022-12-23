46dc52184706a89d8e8ef2f154fe242d2081ee55
alpetrushkin <104225655+alpetrushkin@users.noreply.github.com>
2022-12-23 07:30:15 +0300

    верстка

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
 
             <div className="d-flex flex-wrap">
                <div className="card">
-                  <img width={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
+                  <img width ={133} height={112} src="/img/sneakers/1.jpg" alt="Sneakers"/>
                   <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
                   <div className="d-flex justify-between align-center">
                      <div className="d-flex flex-column">
